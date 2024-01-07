09218a4ad30729b5a45d757a03aba3072ecec46f
Mikayel Sahakyan <mikayel92@gmail.com>
2024-01-08 02:21:54 +0400

    Create a new branch feature/9003 and component NewInStore, use react-slick for slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.scss'
+import NewInStore from './components/NewInStore/NewInStore'
 
 export default function App() {
   return (
     <div>
-      <h1>Furni shop</h1>
+      <NewInStore />
     </div>
   )
 }
